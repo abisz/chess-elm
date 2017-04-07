388de9c7bedec3fbe5e4ef1dfa0b98206600158f
Simon Reinsperger <reinsperger.smn@gmail.com>
2017-04-07 21:31:11 +0200

    test: Bishop Move Tests

--- test/MoveTest.elm
+++ test/MoveTest.elm
@@ -14,6 +14,7 @@ allTests =
     describe "Move Test Suite"
         [ pawnMoveTest
         , knightMoveTest
+        , bishopMoveTest
         ]
 
 
@@ -22,31 +23,40 @@ pawnMoveTest =
     describe "Pawn Move"
         [ test "Default Move" <|
             \() ->
-                Expect.equal True <| testMove "wp4a;" (loc 4 0) (loc 3 0) Pawn White
+                Expect.equal True <|
+                    testMove "wp4a;" (loc 4 0) (loc 3 0) Pawn White
         , test "Can't move backwards" <|
             \() ->
-                Expect.equal False <| testMove "wp4a;" (loc 4 0) (loc 5 0) Pawn White
+                Expect.equal False <|
+                    testMove "wp4a;" (loc 4 0) (loc 5 0) Pawn White
         , test "Can't move diagonal" <|
             \() ->
-                Expect.equal False <| testMove "wp4a;" (loc 4 0) (loc 3 1) Pawn White
+                Expect.equal False <|
+                    testMove "wp4a;" (loc 4 0) (loc 3 1) Pawn White
         , test "Double First Move" <|
             \() ->
-                Expect.equal True <| testMove "wp2a;" (loc 6 0) (loc 4 0) Pawn White
+                Expect.equal True <|
+                    testMove "wp2a;" (loc 6 0) (loc 4 0) Pawn White
         , test "Double Move not possible" <|
             \() ->
-                Expect.equal False <| testMove "wp3a;" (loc 5 0) (loc 2 0) Pawn White
+                Expect.equal False <|
+                    testMove "wp3a;" (loc 5 0) (loc 2 0) Pawn White
         , test "Double Move can't skip figure" <|
             \() ->
-                Expect.equal False <| testMove "wp2a;wp3a;" (loc 6 0) (loc 4 0) Pawn White
+                Expect.equal False <|
+                    testMove "wp2a;wp3a;" (loc 6 0) (loc 4 0) Pawn White
         , test "Can't beat same color" <|
             \() ->
-                Expect.equal False <| testMove "wp2a;wp3a;" (loc 6 0) (loc 5 0) Pawn White
+                Expect.equal False <|
+                    testMove "wp2a;wp3a;" (loc 6 0) (loc 5 0) Pawn White
         , test "Can beat other color straight" <|
             \() ->
-                Expect.equal True <| testMove "wp2a;bp3a;" (loc 6 0) (loc 5 0) Pawn White
+                Expect.equal True <|
+                    testMove "wp2a;bp3a;" (loc 6 0) (loc 5 0) Pawn White
         , test "Can beat other color diagonal" <|
             \() ->
-                Expect.equal True <| testMove "wp2a;bp3b;" (loc 6 0) (loc 5 1) Pawn White
+                Expect.equal True <|
+                    testMove "wp2a;bp3b;" (loc 6 0) (loc 5 1) Pawn White
         ]
 
 
@@ -56,10 +66,12 @@ knightMoveTest =
         [ describe "Basic Moves"
             [ test "Default Success" <|
                 \() ->
-                    Expect.equal True <| testMove "bk5d;" (loc 3 3) (loc 2 1) Knight Black
+                    Expect.equal True <|
+                        testMove "bk5d;" (loc 3 3) (loc 2 1) Knight Black
             , test "Default Failure" <|
                 \() ->
-                    Expect.equal False <| testMove "bk5d;" (loc 3 3) (loc 3 4) Knight Black
+                    Expect.equal False <|
+                        testMove "bk5d;" (loc 3 3) (loc 3 4) Knight Black
             ]
         , test "Jump over Figure" <|
             \() ->
@@ -73,6 +85,36 @@ knightMoveTest =
                 \() ->
                     Expect.equal False <|
                         testMove "bk5d;bp4f;" (loc 3 3) (loc 4 5) Knight Black
+            ]
+        ]
+
+
+bishopMoveTest : Test
+bishopMoveTest =
+    describe "Bishop Moves"
+        [ describe "Basic Move"
+            [ test "Default Success" <|
+                \() ->
+                    Expect.equal True <|
+                        testMove "wb3b;" (loc 5 1) (loc 2 4) Bishop White
+            , test "Default Failure" <|
+                \() ->
+                    Expect.equal False <|
+                        testMove "bb4c;" (loc 4 2) (loc 4 0) Bishop Black
+            ]
+        , test "Jump Over Figure" <|
+            \() ->
+                Expect.equal False <|
+                    testMove "wb6e;wp7d;" (loc 2 4) (loc 0 2) Bishop White
+        , describe "Beat Figure"
+            [ test "Different Color" <|
+                \() ->
+                    Expect.equal True <|
+                        testMove "bb3d;wK5b;" (loc 5 3) (loc 3 1) Bishop Black
+            , test "Same Color" <|
+                \() ->
+                    Expect.equal False <|
+                        testMove "wb8a;wp1h;" (loc 0 0) (loc 7 7) Bishop White
             ]
         ]
 
